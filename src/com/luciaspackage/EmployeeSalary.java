11f7296b4cc35ee5cac9b0e09316cb75ea062acd
LuciaBallova <lucia.ballova.h@gmail.com>
2023-05-10 17:36:35 +0200

    Initial commit

---
+++ src/com/luciaspackage/EmployeeSalary.java
@@ -0,0 +1,49 @@
+package com.luciaspackage;
+
+public class EmployeeSalary {
+    private int baseSalary;
+    private int hourlyRate;
+
+
+    public EmployeeSalary(int baseSalary, int hourlyRate){
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
+    public EmployeeSalary(int baseSalary){
+        this(baseSalary, 0);
+    }
+
+    public int calculateWage(int extraHours){
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public int calculateWage(){
+        return baseSalary;
+    }
+
+    private void setBaseSalary(int baseSalary){
+        if(baseSalary <= 0){
+            throw new IllegalArgumentException("Salary cant be 0 or less");
+            }
+        this.baseSalary = baseSalary;
+    }
+
+
+    private int getBaseSalary(){
+        return baseSalary;
+    }
+
+
+
+    private void setHourlyRate(int hourlyRate) {
+        if(hourlyRate <= 0){
+            throw new IllegalArgumentException("hourlyRate cant be 0 or less");
+        }
+        this.hourlyRate = hourlyRate;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+}
---
+++ src/com/luciaspackage/Main.java
@@ -0,0 +1,18 @@
+package com.luciaspackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var employee1 = new EmployeeSalary(40000,50);
+        int wage1 = employee1.calculateWage(150);
+        System.out.println(wage1);
+
+
+
+    }
+
+
+
+
+
+}
